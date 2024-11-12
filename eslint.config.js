3c0cd72dcc060d04c6cf26735aa86b1a1d21689d
Manikandan B <xqsit94@gmail.com>
2024-11-12 12:38:06 +0530

    update laravel and vue3 dependencies

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{vue,js,jsx,cjs,mjs,ts,tsx,cts,mts}']
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  skipFormatting,
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
+  }
+]
